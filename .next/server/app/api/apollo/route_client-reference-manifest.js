75f107bd079561e401c1020260e39a4c4b49b98d
dhruvtand7 <dhrtandon@gmail.com>
2025-07-15 17:11:57 +0530

    changes

---
+++ .next/server/app/api/apollo/route_client-reference-manifest.js
@@ -0,0 +1,1 @@
+globalThis.__RSC_MANIFEST=(globalThis.__RSC_MANIFEST||{});globalThis.__RSC_MANIFEST["/api/apollo/route"]={"moduleLoading":{"prefix":"/_next/"},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"clientModules":{},"entryCSSFiles":{"C:\\apolloscraper\\apolloscrape\\":[],"C:\\apolloscraper\\apolloscrape\\app\\api\\apollo\\route":[]},"rscModuleMapping":{},"edgeRscModuleMapping":{}}
--- next.config.mjs
+++
@@ -1,7 +0,0 @@
-// next.config.mjs
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // You can add Next.js configuration options here if needed.
-};
-
-export default nextConfig;
